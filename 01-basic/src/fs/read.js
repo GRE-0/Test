b227035dac7b1921f902106f610bd3a988341323
DESKTOP-2OR8TL9\GRE_ <eter>
2024-08-12 12:02:44 +0200

    "Real Test 2"

--- 01-basic/src/fs/read.js
+++ 01-basic/src/fs/read.js
@@ -1,7 +1,23 @@
-const read = async () => {
-    // Diese Funktion wird verwendet, um asynchron Daten zu lesen.
+import {fileURLtoPath} from 'url'
+import path, {join} from "path";
+import * as fs from "fs"
 
-    // Schreibe hier deinen Code
+const filename = "fresh.txt";
+const folder = "files";
+const errorMessage = "FS operation failed";
+const pathToFile = join(fileURLtoPath(import.meta.url), "...", folder. filename)
+const data = new Uint8Array(Buffer.from(successText))
+
+
+const read = async () => {
+    try{
+   await fs.promises.readFile(pathToFile)
+    }
+    catch(error)
+    {
+        throw new error(errorMessage);
+    }
+
 };
 
 // Warte auf die Ausführung der read-Funktion
